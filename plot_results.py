293289140873077ffb251dda59ec946db2c27022
particle <54845098+particle1331@users.noreply.github.com>
2021-12-30 22:33:50 +0800

    Upload files.

---
+++ plot_results.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pandas as pd
+import glob
+
+dfs = []
+for filename in glob.glob('results*.csv'):
+    dfs.append(pd.read_csv(filename, index_col=0))
+
+result_all = pd.concat(dfs)
+result_all = result_all.sort_values("20", ascending=False)
+fig, ax = plt.subplots(1, 1, figsize=(8, 6), dpi=200)
+for key in result_all.index:
+    ax.plot([5, 10, 15, 20], result_all.loc[key].values, label=key)
+
+plt.legend()
+plt.grid()
+plt.xlabel("Network depth")
+plt.ylabel("Training time (s)")
+plt.title(f"TensorFlow {tf.__version__} Benchmark (Macbook Air 2020 M1)")
+plt.savefig("results_plot.png")
---
+++ run.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+
+# Get CIFAR10 data; do basic preprocessing
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
+X_train_scaled = X_train / 255.0
+y_train_encoded = tf.keras.utils.to_categorical(y_train, num_classes=10, dtype='float32')
+
+# Define model constructor
+def get_model(depth):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(32, 32, 3)))
+    for _ in range(depth):
+        model.add(tf.keras.layers.Dense(1024, activation='relu'))
+    model.add(tf.keras.layers.Dense(10, activation='sigmoid'))
+    model.compile(optimizer='SGD', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+if __name__ == "__main__":
+    import time
+    import pandas as pd
+    
+    network_depth = [5, 10, 15, 20]
+    results = { depth: {} for depth in network_depth }
+    for depth in network_depth:
+
+        # Test with CPU
+        with tf.device('/CPU:0'):
+            cpu_start_time = time.time()
+            model_cpu = get_model(depth)
+            model_cpu.fit(X_train_scaled, y_train_encoded, epochs=3)
+            results[depth]['CPU'] = time.time() - cpu_start_time
+
+        # Test with GPU
+        with tf.device('/GPU:0'):
+            gpu_start_time = time.time()
+            model_gpu = get_model(depth)
+            model_gpu.fit(X_train_scaled, y_train_encoded, epochs=3)
+            results[depth]['M1'] = time.time() - gpu_start_time
+
+        # Test with default env
+        default_start_time = time.time()
+        model_default = get_model(depth)
+        model_default.fit(X_train_scaled, y_train_encoded, epochs=3)
+        results[depth]['Default'] = time.time() - default_start_time
+
+    # Save results
+    pd.DataFrame(results).to_csv('results_local.csv', index=True)
