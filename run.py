ffe66e5835277a6b1d5b62f8b8d6e18701144d6c
particle <54845098+particle1331@users.noreply.github.com>
2022-01-02 19:40:13 +0800

    Refactor benchmark test indices.
    Refactor benchmark test indices.

--- run.py
+++ run.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 import pandas as pd
 import time
 from benchmarks import benchmarks
-from utils import get_benchmark, time_model
+from utils import get_benchmark, time_model_training
 
 
 def run(env_name: str, benchmark_name: str) -> dict:
     benchmark = get_benchmark(benchmark_name)
-    results = {depth: {} for depth in benchmark.test_indices}
-    for test_index in results.keys():
+    results = {}
+    for test_index in benchmark.test_indices:
         model = benchmark.get_model(test_index)
-        results[test_index][env_name] = time_model(model)
+        results[test_index] = {env_name: time_model_training(model)}
 
     pd.DataFrame(results).to_csv(f'results/{benchmark_name}_{env_name}.csv', index=True)
     return results
--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ X_train_scaled = X_train / 255.0
 y_train_encoded = tf.keras.utils.to_categorical(y_train, num_classes=10, dtype='float32')
 
 # Compute training time for training 3 epochs
-def time_model(model):
+def time_model_training(model):
     start_time = time.time()
     model.fit(X_train_scaled, y_train_encoded, epochs=3)
     return time.time() - start_time
